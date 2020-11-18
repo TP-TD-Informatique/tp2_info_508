0caf8227623b7d8b4baf14c95588371727076684
Kevin Traini <kevin@ktraini.com>
2020-11-18 18:23:34 +0100

    Partie 5

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ app.use("/", express.static(__dirname + "/public"));
 app.listen("11009", function() {
     console.log("L'application est lancée...");
 });
+
+
+const MongoClient = require("mongodb").MongoClient;
+var db = null;
+MongoClient.connect(
+    "mongodb://info508:info508@localhost:27017/info508",
+    { useUnifiedTopology: true },
+    function(err, client) {
+        if (err != null) {
+            console.log("\033[31m>>> Erreur lors de la connexion à MongoDB\033[0m");
+        } else {
+            console.log("Connecté à MongoDB");
+            db = client.db("info508");
+        }
+    }
+);
