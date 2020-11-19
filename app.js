c82a079884f0bf3dc6895042af79574bc111e6fa
gashmob <kevin@ktraini.com>
2020-11-19 08:48:41 +0000

    Partie 6

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.listen("11009", function() {
 });
 
 
+// Connexion à MongoDB
 const MongoClient = require("mongodb").MongoClient;
 var db = null;
 MongoClient.connect(
@@ -23,3 +24,27 @@ MongoClient.connect(
         }
     }
 );
+
+
+// Ajout de la route /distance/{ville}
+app.use("/distance/:city", function(req, res) {
+	const path = "/route.json?stops=Bourget-Du-Lac|" + encodeURI(req.params.city);
+	
+	var data = "";
+	require("https").request({
+		host: "fr.distance24.org",
+		path: path,
+		port: 443,
+		method: "GET"
+	}, function(result) {
+		result.setEncoding("utf-8");
+		result.on("data", (chunk) => {
+			data += chunk;
+		});
+		result.on("end", () => {
+			const d = JSON.parse(data);
+			console.log("\033[32m" + path + " --> " + d.distance + "\033[0m");
+			res.status(200).send(`${d.distance}`);
+		});
+	}).end();
+});
---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const request = function(url, id) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(id).innerText = this.responseText;
+		}
+	};
+	xmlhttp.open("GET", url);
+	xmlhttp.send();
+};
+request("http://mongo.learninglab.eu/gr1_info9/distance/Chambery", "test");
